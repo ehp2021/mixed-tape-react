790b6c9efbbab190f1e5a7163893b50e29055e01
ehp2021 <emmahpaik@gmail.com>
2021-11-22 12:13:58 -0500

    search working

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,13 +11,15 @@ const redirect_uri = 'http://localhost:3001/callback';
 // process.env.REDIRECT_URI; // redirect uri
 const frontend_uri = 'http://localhost:3000';
 // process.env.FRONTEND_URI;
+const SpotifyWebApi = require("spotify-web-api-node")
 
 const generateRandomString = function(length) {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));}
-  return text;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 };
 const stateKey = 'spotify_auth_state';
 
@@ -25,35 +27,56 @@ const login = (req, res) => {
     const state = generateRandomString(16);
     res.cookie(stateKey, state);
 
-      const scope = [
-     "playlist-modify-private playlist-read-private playlist-modify-public playlist-read-collaborative user-read-playback-state user-modify-playback-state app-remote-control streaming user-read-email user-read-private user-top-read"
-   ]
-  
+    const scope = [
+        "playlist-modify-private playlist-read-private playlist-modify-public playlist-read-collaborative user-read-playback-state user-modify-playback-state app-remote-control streaming user-read-email user-read-private user-top-read"
+    ]
+
     res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        redirect_uri: redirect_uri,
-        scope: scope,
-        state: state
-      }));
-  };
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            redirect_uri: redirect_uri,
+            scope: scope,
+            state: state
+        }));
+};
+
+const login = (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: redirect_uri,
+        clientId: client_id,
+        clientSecret: client_secret,
+    })
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            })
+        })
+        .catch(err => {
+            res.sendStatus(400)
+        })
+}
 
 
-  const getToken = (req, res) => {
-    const code = req.query.code || null;
-  
-    axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      data: querystring.stringify({
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirect_uri
-      }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+const getToken = (req, res) => {
+        const code = req.query.code || null;
+
+        axios({
+                    method: 'post',
+                    url: 'https://accounts.spotify.com/api/token',
+                    data: querystring.stringify({
+                        grant_type: 'authorization_code',
+                        code: code,
+                        redirect_uri: redirect_uri
+                    }),
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded',
+                        Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
       },
     })
     .then(response => {
@@ -75,4 +98,4 @@ const login = (req, res) => {
     
 
 
-  module.exports = { login, getToken};
+  module.exports = { login, getToken};(r)
--- server/controllers/refresh.js
+++ server/controllers/refresh.js
@@ -1,33 +1,39 @@
-
-
+const SpotifyWebApi = require("spotify-web-api-node")
 const querystring = require('query-string');
 const axios = require('axios');
 
 const client_id = process.env.SPOTIFY_CLIENT_ID; // client id
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // secret
-
+const client_id = 'ac468ec84f8d4070843bb0ce3cfd5c95';
+//process.env.SPOTIFY_CLIENT_ID; // client id
+const client_secret = 'e35c37b3eb0f4fbfb45f71456255a6e7';
+// process.env.SPOTIFY_CLIENT_SECRET; // secret
+const redirect_uri = 'http://localhost:3001/callback';
+// process.env.REDIRECT_URI; // redirect uri
+const frontend_uri = 'http://localhost:3000';
+// process.env.FRONTEND_URI;
 
 const getRefreshToken = (req, res) => {
-    const { refresh_token } = req.query;
-  
-    axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      data: querystring.stringify({
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
-      }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
-      },
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: redirect_uri,
+        clientId: client_id,
+        clientSecret: client_secret,
+        refreshToken,
     })
-      .then(response => {
-        res.send(response.data);
-      })
-      .catch(error => {
-        res.send(error);
-      });
-  };
 
-  module.exports = { getRefreshToken };
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+};
+
+module.exports = { getRefreshToken };
